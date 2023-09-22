092a2bb00464e0fbb7e259680f4f8665fb1f897d
RendyAFS <arendy724@gmail.com>
2023-09-22 13:58:01 +0700

    fix minor dan fitur

--- resources/views/katalogmahal.blade.php
+++ resources/views/katalogmahal.blade.php
@@ -18,6 +18,9 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="d-flex justify-content-end mb-4">
+                    <a href="https://www.compress2go.com/id/kompres-jpg" target="__blank" class="btn btn-danger me-2 shadow">
+                        <i class="bi bi-images me-2"></i> Kompress IMG
+                    </a>
                     <a href="{{ route('katalog1.index') }}" target="__blank" class="btn btn-success me-2 shadow">
                         <i class="bi bi-eye me-2"></i> Lihat Katalog
                     </a>
